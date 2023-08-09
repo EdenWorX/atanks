19069ab7605e3249c11981f25f3d1105bd4fa719
Sven Eden <sven@eden-worx.com>
2023-08-09 12:08:15 +0200

    Remove orphaned dependency files
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- dep/main.d
+++
@@ -1,4 +0,0 @@
-obj/main.o dep/main.d : src/main.cpp src/main.h src/debug.h src/globaltypes.h \
- src/wrap_dirent.h src/externs.h src/globaldata.h src/box.h \
- src/environment.h src/gfxData.h src/bitmap.h src/network.h src/text.h \
- src/spinlock.h
