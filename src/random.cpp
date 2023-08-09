b10c98d0b8d670d2405c495fd80a749a5bb5a1af
Sven Eden <sven@eden-worx.com>
2023-08-09 11:05:57 +0200

    Cleanup and clang-tidy fixes in src/random.*
    Signed-off-by: Sven Eden <sven@eden-worx.com>

--- src/random.cpp
+++ src/random.cpp
@@ -6,24 +6,22 @@
 
 #include <random>
 
-
 /** @brief get a random number between 0 and @arg u
  *
  * Noon-extreme values are preferred, based on a simple cubic function.
  *
  * @param[in] u The upper limit
  * @return a random number between 0 and @arg u
-**/
+ **/
 double central_rand( double u ) {
 	double const x = static_cast< double >( get_rand() ) / static_cast< double >( INT32_MAX ) - 0.5; // [-.5,+.5]
 	return u * ( 0.5 - ( x * x * x ) * 4.0 );
 }
 
-
 /** @brief get a full range random number, substituting srand()/rand()
  *
  * @return a random number between 0 and INT_MAX
-**/
+ **/
 int32_t get_rand() {
 	static thread_local std::random_device rand_dev{};
 	static thread_local std::minstd_rand   rand_gen{ rand_dev() };
